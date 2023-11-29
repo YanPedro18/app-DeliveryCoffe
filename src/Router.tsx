90bd0e7a2c4b3c85f9d6a04603ffc1d61fb3e553
Yan Pedro <68964255+YanPedro18@users.noreply.github.com>
2023-11-29 12:36:53 -0300

    feat: route creation component for pages

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,7 +2,7 @@ import { Routes, Route } from "react-router-dom"
 import Home from "./Pages/Home"
 import Carrinho from "./Pages/Carrinho"
 import DefaultLayout from "./layouts/DefaultLayout"
-import Finally from "./Pages/Finally"
+import Final from "./Pages/Final"
 
 
 function Router() {
@@ -11,7 +11,7 @@ function Router() {
        <Route path="/" element={<DefaultLayout />}>
         <Route path="/" element={<Home/>}></Route>
         <Route path="/Carrinho" element={<Carrinho/>}></Route>
-        <Route path="/Finally" element={<Finally/>}></Route>
+        <Route path="/Final" element={<Final/>}></Route>
        </Route>
     </Routes>
   )
