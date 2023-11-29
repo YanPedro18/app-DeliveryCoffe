3f5865e40b723ee2ba34600c6ab10702eb6a2e08
Yan Pedro <68964255+YanPedro18@users.noreply.github.com>
2023-11-29 11:20:55 -0300

    Componente de página principal home

---
+++ src/Pages/Home/index.tsx
@@ -0,0 +1,18 @@
+
+import Card from '../../components/Card';
+import { HomeContainer } from './styles';
+import Intro from '../../components/Intro';
+
+
+function Home() {
+
+
+    return (
+        <HomeContainer>
+                <Intro />
+                <Card />
+        </HomeContainer>
+    )
+}
+
+export default Home
