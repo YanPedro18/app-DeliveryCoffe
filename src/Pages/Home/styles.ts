5b96ec602bac76184fb48326d5292e5ee2d75339
Yan Pedro <68964255+YanPedro18@users.noreply.github.com>
2023-11-29 11:21:17 -0300

    home page styling file

---
+++ src/Pages/Home/styles.ts
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+
+
+export const HomeContainer = styled.section`
+    margin: 0 auto;
+    text-align: center;
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    justify-content: center;
+
+    & main > h1 {
+        text-align: left;
+    }
+
+
+`
