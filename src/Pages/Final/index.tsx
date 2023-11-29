58d800fb9626c65b9e1d3b45a97b6b943dcb2f89
Yan Pedro <68964255+YanPedro18@users.noreply.github.com>
2023-11-29 12:40:26 -0300

    (Directory) final page styling file

--- src/Pages/Final/index.tsx
+++ src/Pages/Final/index.tsx
@@ -1,12 +1,20 @@
 import { FinnalyContainer } from "./styles"
 import imgFinally from "../../assets/Illustration.svg"
+import { useContext, useEffect } from "react"
+import { coffeContext } from "../../context"
 
 function Final() {
+  const {finallyCoffe} = useContext(coffeContext)
+
+  useEffect(() => {
+    finallyCoffe()
+  }, [])
+  
   return (
     <>
         <FinnalyContainer>
             <img src={imgFinally} alt="" />
-            <h4>Compra concluída</h4>
+            <h4>Compra concluída !</h4>
         </FinnalyContainer>
     </>
   )
